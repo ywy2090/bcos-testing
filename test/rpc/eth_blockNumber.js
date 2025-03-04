0884a2a31a4bbb8d59beae5eb200192075d39093
octopus <912554887@qq.com>
2025-03-04 14:22:31 +0800

    format code

--- test/rpc/eth_blockNumber.js
+++ test/rpc/eth_blockNumber.js
@@ -1,17 +1,17 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("eth_blockNumber", function() {
-  it("should return current block number", async function() {
+describe("eth_blockNumber", function () {
+  it("should return current block number", async function () {
     // 直接使用ethers.js的provider调用接口
     const blockNumber = await ethers.provider.getBlockNumber();
-    
+
     // 也可以使用原始RPC调用
     const blockNumberRaw = await ethers.provider.send("eth_blockNumber", []);
-    
+
     console.log(`Current block number: ${blockNumber}`);
     console.log(`Current block number (raw): ${parseInt(blockNumberRaw, 16)}`);
-    
+
     // 验证结果
     expect(blockNumber).to.be.a("number");
     expect(blockNumber).to.equal(parseInt(blockNumberRaw, 16));
--- test/rpc/eth_getTransactionCount.js
+++ test/rpc/eth_getTransactionCount.js
@@ -1,20 +1,20 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("eth_getTransactionCount", function() {
+describe("eth_getTransactionCount", function () {
   let testAccount;
-  
-  before(async function() {
+
+  before(async function () {
     [testAccount] = await ethers.getSigners();
   });
-  
-  it("should return account nonce", async function() {
+
+  it("should return account nonce", async function () {
     const nonce = await ethers.provider.getTransactionCount(testAccount.address);
     const nonceRaw = await ethers.provider.send("eth_getTransactionCount", [
       testAccount.address,
       "latest"
     ]);
-    
+
     expect(nonce).to.equal(parseInt(nonceRaw, 16));
   });
 });
