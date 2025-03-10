504f8ebad92ddc12cf76ec93fc18db32729db312
octopus <912554887@qq.com>
2025-03-10 15:54:50 +0800

    add local host

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 // require("hardhat-docgen");
 require("dotenv").config();
 
+const LOCAL_URL = process.env.LOCAL_HOST_URL || "";
 const BCOSNET_URL = process.env.BCOS_HOST_URL || "";
 const MAINNET_URL = process.env.MAINNET_URL || "";
 const SEPOLIA_URL = process.env.SEPOLIA_URL || "";
 const GOERLI_URL = process.env.GOERLI_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY.length > 0 ? process.env.PRIVATE_KEY.split(',') : [];
 
-console.log("PRIVATE_KEY:", PRIVATE_KEY);
+console.log(" :", PRIVATE_KEY);
 console.log("BCOSNET_URL:", BCOSNET_URL);
 
 /** @type {import('hardhat/config').HardhatUserConfig} */
@@ -36,6 +37,17 @@ const config = {
   networks: {
     bcosnet: {
       url: BCOSNET_URL,
+      accounts: PRIVATE_KEY.length > 0 ? PRIVATE_KEY : [
+        "b0b9d33d8558ffb74cfa501426a1652bd4cb3452d49faad9b235c42f66d39e33",
+        "82e8df71b5066e0fd42c527317fba6ba5f6b60c5546e92c904efeaa8ec6a2d59",
+        "5ab8223a7b6656e939a4ebf233e3bcf8e230163d4048b9cb6380e1d9ad555ba9",
+        "5ab8223a7b6656e939a4ebf233e3bcf8e230163d4048b9cb6380e1d9ad555ba9"
+      ],
+      chainId: 20200
+    },
+
+    localnet: {
+      url: LOCAL_URL,
       accounts: PRIVATE_KEY.length > 0 ? PRIVATE_KEY : [
         "b0b9d33d8558ffb74cfa501426a1652bd4cb3452d49faad9b235c42f66d39e33",
         "82e8df71b5066e0fd42c527317fba6ba5f6b60c5546e92c904efeaa8ec6a2d59",
